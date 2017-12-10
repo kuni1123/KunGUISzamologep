adee0f382bc0289427933e7ad4fd0800025677a0
GeorgeCooney <georgecooney@users.noreply.github.com>
2017-12-10 19:37:29 +0100

    Osztály létrehozva

---
+++ src/Ablak.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Ablak {
+    private JPanel panel1;
+    private JButton bt_szamol;
+    private JTextField tf_szamok;
+    private JComboBox cb_elvalasztoKarakterek;
+}
