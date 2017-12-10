5fa8e7fdca62b5a119e1eff8af455ea8b66d07e5
GeorgeCooney <georgecooney@users.noreply.github.com>
2017-12-10 19:58:36 +0100

    Tetszőleges számú szám

--- src/Ablak.java
+++ src/Ablak.java
@@ -1,8 +1,58 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ablak {
     private JPanel panel1;
+
+
+    int add(String[] szamok){
+        for (int i=0;i<szamok.length;i++){
+                osszeg += Integer.parseInt(szamok[i]);
+        }
+        return osszeg;
+    }
+
+    public Ablak() {
+        bt_szamol.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int osszeg = 0;
+
+                try{
+                    String[] szamok = tf_szamok.getText().split(",");
+                    osszeg = add(szamok);
+                        JOptionPane.showMessageDialog(null, "A beírt számok összege: " + osszeg);
+                }catch(Exception ex){
+                    JOptionPane.showMessageDialog(null,"Nem a megfelelő elválasztó karaktert használta!");
+                }
+            }
+        });
+    }
+
+    public JPanel getPanel1() {
+        return panel1;
+    }
+
     private JButton bt_szamol;
     private JTextField tf_szamok;
     private JComboBox cb_elvalasztoKarakterek;
+    public int osszeg=0;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Számológép");
+        frame.setContentPane(new Ablak().getPanel1());
+        Dimension meret = new Dimension(300, 200);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+        frame.setLocation(((int) width / 2) - meret.width / 2, ((int) height / 2) - meret.height / 2);
+        frame.setMinimumSize(meret);
+        frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
+
+
