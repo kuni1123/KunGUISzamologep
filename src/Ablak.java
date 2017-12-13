f81ec13ac79ca851cc4665530f76175c67d7936a
GeorgeCooney <georgecooney@users.noreply.github.com>
2017-12-13 18:23:36 +0100

    Kivételkezelés

--- src/Ablak.java
+++ src/Ablak.java
@@ -9,18 +9,24 @@ public class Ablak {
     private JButton bt_szamol;
     private JTextField tf_szamok;
     private JComboBox cb_elvalasztoKarakterek;
-    public int osszeg=0;
+    public String osszeg="";
 
     String minuszok = "Nem írhatóak be negatív számok. Az ön által megadott negatív számok: ";
-    int add(String[] szamok){
-        osszeg=0;
-        for (int i=0;i<szamok.length;i++){
-            if(Integer.parseInt(szamok[i])<0) {
-                minuszok+=" "+szamok[i];
-                minuszDarab++;
+    String add(String[] szamok){
+        int osszeg2=0;
+        try {
+            for (int i = 0; i < szamok.length; i++) {
+                if (Integer.parseInt(szamok[i]) < 0) {
+                    minuszok += " " + szamok[i];
+                    minuszDarab++;
+                } else
+                    osszeg2 += Integer.parseInt(szamok[i]);
             }
-            else
-                osszeg += Integer.parseInt(szamok[i]);
+            osszeg = Integer.toString(osszeg2);
+        }
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null,"Hiba történt: "+ex.getMessage());
+            osszeg="Hiba";
         }
         return osszeg;
     }
@@ -29,7 +35,7 @@ public class Ablak {
         bt_szamol.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int osszeg;
+                String osszeg="";
                 Character[] karakterek = {',','!','%','/','=',':','_','>',';','<'};
                 String karakter = ""+karakterek[cb_elvalasztoKarakterek.getSelectedIndex()];
                 try{
@@ -38,10 +44,13 @@ public class Ablak {
                     if(minuszDarab>0){
                         JOptionPane.showMessageDialog(null,minuszok+"");
                     }else {
-                        JOptionPane.showMessageDialog(null, "A beírt számok összege: " + osszeg);
+                        if(osszeg!="Hiba")
+                            JOptionPane.showMessageDialog(null, "A beírt számok összege: " + osszeg);
                     }
                 }catch(Exception ex){
+                    if(osszeg!="Hiba")
                     JOptionPane.showMessageDialog(null,"Nem a megfelelő elválasztó karaktert használta!");
+
                 }
             }
         });
